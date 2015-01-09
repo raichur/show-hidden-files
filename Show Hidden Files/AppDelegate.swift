d3fb0833e2cb76ab6164b9d4ca3b3fe008d1c6b0
Joshua Raichur <hello@joshuaraichur.com>
2015-01-09 13:37:05 +0530

    Initial Commit

---
+++ Show Hidden Files/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Show Hidden Files
+//
+//  Created by Joshua Raichur on 1/9/15.
+//  Copyright (c) 2015 Joshua Raichur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Show Hidden FilesTests/Show_Hidden_FilesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Show_Hidden_FilesTests.swift
+//  Show Hidden FilesTests
+//
+//  Created by Joshua Raichur on 1/9/15.
+//  Copyright (c) 2015 Joshua Raichur. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Show_Hidden_FilesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
